cc62e79832c21d54644b63988306a3505657b48b
Mahmoud Medhat <hhetlar120@gmail.com>
2022-09-30 17:38:18 +0200

    fdfd

--- commands.js
+++ commands.js
@@ -382,7 +382,6 @@ async function cmd(conn, mek) {
                 break
             case 'send':
                 try {
-                    console.log(q);
                     // let r = (Math.random() + 1).toString(36).substring(7);
                     // const rand = r + '.mp3'
                     const media = request(q).pipe(fs.createWriteStream('/tmp/' + q));
