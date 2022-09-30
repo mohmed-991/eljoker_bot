7149893a5de0f7c15a92cc9701c88bea02e1d92d
Mahmoud Medhat <hhetlar120@gmail.com>
2022-09-30 17:41:38 +0200

    hd

--- commands.js
+++ commands.js
@@ -384,14 +384,14 @@ async function cmd(conn, mek) {
                 try {
                     // let r = (Math.random() + 1).toString(36).substring(7);
                     // const rand = r + '.mp3'
-                    const media = request(q).pipe(fs.createWriteStream('/tmp/' + q));
+                    const media = request(q).pipe(fs.createWriteStream('./tmp/' + q));
                     const media1 = media.on("finish", () => {
-                        return fs.statSync('/tmp/' + q).size;
+                        return fs.statSync('./tmp/' + q).size;
                     });
                     console.log(media1);
                     await conn.sendMessage(from, { text: media1 }, { quoted: mek })
                     try {
-                        fs.unlinkSync('/tmp/' + q)
+                        fs.unlinkSync('./tmp/' + q)
                     } catch (err) {
                         console.error(err)
                     }
