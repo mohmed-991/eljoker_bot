1319c9ff3f4bd9b011a414fec3f5a1191d7c611c
Mahmoud Medhat <hhetlar120@gmail.com>
2022-10-01 19:42:12 +0200

    ds

--- commands.js
+++ commands.js
@@ -131,6 +131,8 @@ async function cmd(conn, mek) {
                     case 'imageMessage':
                         const cstic = await conn.sendMessage(from, { text: 'creating جاري صناعة الملصق' }, { quoted: mek })
                         var nameJpg = getRandom('')
+                        const v = sms(conn, mek)
+                        const isQuotedViewOnce = v.quoted ? (v.quoted.type === 'viewOnceMessage') : false
                         const isQuotedImage = v.quoted ? ((v.quoted.type === 'imageMessage') || (isQuotedViewOnce ? (v.quoted.msg.type === 'imageMessage') : false)) : false
                         isQuotedImage ? await v.quoted.download(nameJpg) : await v.download(nameJpg)
                         var stik = await imageToWebp(nameJpg + '.jpg')
